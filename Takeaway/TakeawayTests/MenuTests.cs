5f820c2a21eec47ce27b48221b72d796f8d5bbd0
LeoRoma <xiajtn@gmail.com>
2020-05-28 10:18:22 +0100

    Tested constructor of Menu class

---
+++ Takeaway/TakeawayTests/MenuTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using Takeaway;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Takeaway.Tests
+{
+    public class MenuTests
+    {
+        private Dish[] _dishes;
+        private Menu _menu;
+
+        [Fact()]
+        public void MenuTest()
+        {
+            _dishes = new Dish[]
+            {
+                new Dish("Margherita", 5.00),
+                new Dish("Boscaiola", 6.50),
+                new Dish("Capricciosa", 7.00)
+            };
+
+            _menu = new Menu(_dishes);
+        }
+
+        [Fact()]
+        public void PrintTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
